e7e240fc4e73da5d02f449ed603ffa113123de7b
Brandon Castro <castroyou999@gmail.com>
2023-02-06 13:30:07 -0500

    commit tree 80871623df184604fd0a3fc70c3c33684a28408e

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :rescue_record_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items.all
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item , include: :user, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def rescue_record_not_found(invalid)
+  #   if !User.find_by(id: params[:user_id]) 
+  #     error = "user not found"
+  #   elsif !Item.find_by(id: params[:id])
+  #     error = "item not found"
+  #   end
+  #   render json: {errors: error}, status: :not_found
+    render json: {errors: invalid}, status: :not_found
   end
 
 end
